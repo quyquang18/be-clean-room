2f3e9824f13946222c91fab744cdc5c46ab230a9
quyquang1809 <accriseofking2@gmail.com>
2023-05-04 12:39:25 +0700

    update-relation

--- src/models/device.js
+++ src/models/device.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Device.belongsTo(models.Room, { foreignKey: "roomId", targetKey: "id" });
-      User.belongsTo(models.Allcode, {
+      Device.belongsTo(models.Allcode, {
         foreignKey: "typeDevice",
         targetKey: "keyMap",
         as: "typeDeviceData",
