e1668f8e8ec4d5d6c23de972d47da6d74eb6cab4
quyquang1809 <accriseofking2@gmail.com>
2023-05-15 08:38:56 +0700

    jwt

--- src/server.js
+++ src/server.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 import cors from 'cors';
 import express from "express";
 import bodyParser from "body-parser";
-
+import cookieParser from "cookie-parser";
 import viewEngine from "./config/viewEngine";
-import initWebRoutes from './route/web';
+import initWebRoutes from "./route/web";
 import connectDB from "./config/connectDB";
 let app = express();
 app.use(cors({ credentials: true, origin: process.env.URL_REACT }));
+console.log(process.env.URL_REACT);
 //config app
 
 // app.use(bodyParser.json());
@@ -15,7 +16,15 @@ app.use(cors({ credentials: true, origin: process.env.URL_REACT }));
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(cookieParser());
+app.get("/", function (req, res) {
+  // Cookies that have not been signed
+  console.log("Cookies: ", req.cookies);
 
+  // Cookies that have been signed
+  console.log("Signed Cookies: ", req.signedCookies);
+});
+  
 viewEngine(app);
 initWebRoutes(app);
 connectDB();
--- src/services/sensorService.js
+++ src/services/sensorService.js
@@ -182,4 +182,5 @@ module.exports = {
   getValueSensorByTime: getValueSensorByTime,
   updateValueThreshold: updateValueThreshold,
   getValueThreshold: getValueThreshold,
+  createNewValueSensor: createNewValueSensor,
 };
